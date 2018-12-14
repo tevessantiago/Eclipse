a684adc052919bde2d92ddb82693b1aad872e690
tevessantiago <39637063+tevessantiago@users.noreply.github.com>
2018-12-13 23:41:30 -0300

    Hasta clase 28

---
+++ PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/Coche.java
@@ -0,0 +1,21 @@
+package poo;
+
+public class Coche {
+	
+	int ruedas = 4;
+	int largo; //cm
+	int ancho; //cm
+	int motor; //cm cubico
+	int peso; //kg
+	
+	public Coche(){
+		
+		ruedas 	= 4;
+		largo 	= 2000;
+		ancho 	= 300;
+		motor 	= 1600;
+		peso 	=  500;
+		
+	}
+
+}
---
+++ PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/Uso_Coche.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		
+		Coche Renault = new Coche(); //INSTANCIAR UNA CLASE. EJEMPLAR DE CLASE.
+		
+		System.out.println("Este coche tiene " + Renault.ruedas + " ruedas.");
+
+	}
+
+}
