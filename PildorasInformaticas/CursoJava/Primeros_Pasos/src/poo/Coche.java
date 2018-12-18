9a5dd5ea06bbbb8e4575df44eea5f38382277fbd
tevessantiago <39637063+tevessantiago@users.noreply.github.com>
2018-12-18 15:29:43 -0300

    Clase 33

--- PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/Coche.java
+++ PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/Coche.java
@@ -46,7 +46,7 @@ public class Coche {
 	
 	public void configura_asientos(String asientos_cuero){ //SETTER
 		
-		if(asientos_cuero == "si"){
+		if(asientos_cuero.equalsIgnoreCase("si")){
 			this.asientos_cuero = true;
 		}else{
 			this.asientos_cuero = false;
@@ -54,7 +54,7 @@ public class Coche {
 		
 	}
 	
-	public String dime_asientos(){
+	public String dime_asientos(){ //GETTER
 		
 		if(asientos_cuero == true){
 			
@@ -64,6 +64,76 @@ public class Coche {
 			
 			return "El coche tiene asientos de serie";
 		}
+		
+	}
+	
+	public void configura_climatizador(String climatizador){ //SETTER
+		
+		if(climatizador.equalsIgnoreCase("si")){
+			
+			this.climatizador = true;
+			
+		}else{
+			
+			this.climatizador = false;
+			
+		}
+		
+	}
+	
+	public String dime_climatizador(){
+		
+		if(climatizador == true){
+			
+			return "El coche incorpora climatizador";
+			
+		}else{
+			
+			return "El coche lleva aire acondicionado";
+			
+		}
+		
+	}
+	
+	public String dime_peso_coche(){//SETTER + GETTER no se recomienda.
+		
+		int peso_carroceria = 500;
+		
+		peso_total = peso_plataforma + peso_carroceria;
+		
+		if(asientos_cuero == true){
+			
+			peso_total = peso_total + 50;
+			
+		}
+		
+		if(climatizador == true){
+			
+			peso_total = peso_total + 20;
+			
+		}
+		
+		return "El peso del coche es " + peso_total;
+		
+	}
+	
+	public int precio_coche(){ //GETTER
+		
+		int precio_final = 10000;
+		
+		if(asientos_cuero == true){
+			
+			precio_final += 2000;
+			
+		}
+		
+		if(climatizador == true){
+			
+			precio_final += 1500;
+			
+		}
+		
+		return precio_final;
 		
 	}
 
