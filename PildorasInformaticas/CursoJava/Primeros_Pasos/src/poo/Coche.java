6e09a76a2736d779a9ae0d1fbfa92e9d1aef5001
tevessantiago <39637063+tevessantiago@users.noreply.github.com>
2018-12-18 13:10:26 -0300

    Clase 30

--- PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/Coche.java
+++ PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/Coche.java
@@ -2,19 +2,40 @@ package poo;
 
 public class Coche {
 	
-	int ruedas = 4;
-	int largo; //cm
-	int ancho; //cm
-	int motor; //cm cubico
-	int peso; //kg
+	private int ruedas = 4;
+	private int largo; //cm
+	private int ancho; //cm
+	private int motor; //cm cubico
+	private int peso_plataforma; //kg
+	String color;
+	int peso_total;
+	boolean asientos_cuero, climatizador;
 	
 	public Coche(){
 		
-		ruedas 	= 4;
-		largo 	= 2000;
-		ancho 	= 300;
-		motor 	= 1600;
-		peso 	=  500;
+		ruedas 				= 4;
+		largo 				= 2000;
+		ancho 				= 300;
+		motor 				= 1600;
+		peso_plataforma 	=  500;
+		
+	}
+	
+	public String dime_largo(){ // GETTER
+		
+		return "El largo del coche es " + largo;
+		
+	}
+	
+	public void establece_color(){ //SETTER
+		
+		color = "azul";
+		
+	}
+	
+	public String dime_color(){
+		
+		return "El color del coche es " + color;
 		
 	}
 
--- PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/Uso_Coche.java
+++ PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/Uso_Coche.java
@@ -4,10 +4,12 @@ public class Uso_Coche {
 
 	public static void main(String[] args) {
 		
-		Coche Renault = new Coche(); //INSTANCIAR UNA CLASE. EJEMPLAR DE CLASE.
+		Coche micoche = new Coche();
 		
-		System.out.println("Este coche tiene " + Renault.ruedas + " ruedas.");
-
+		//micoche.establece_color();
+		
+		System.out.println(micoche.dime_color());
+		
 	}
 
 }
