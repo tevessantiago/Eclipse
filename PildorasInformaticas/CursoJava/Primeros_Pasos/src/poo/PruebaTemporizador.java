db2695832baf30509976a3752fb9a12fefd3b59d
tevessantiago <39637063+tevessantiago@users.noreply.github.com>
2019-02-11 15:45:53 -0300

    Clase 52

---
+++ PildorasInformaticas/CursoJava/Primeros_Pasos/src/poo/PruebaTemporizador.java
@@ -0,0 +1,42 @@
+/**
+ * Clase 52
+ * Segundo ejemplo de utilización de Interfaces.
+ */
+package poo;
+
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+
+public class PruebaTemporizador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DameLaHora oyente = new DameLaHora();
+		
+		Timer miTemporizador = new Timer(5000, oyente);
+		
+		miTemporizador.start();
+		
+		JOptionPane.showMessageDialog(null, "Pulsa 'OK' para detener.");
+		
+		System.exit(0);
+
+	}
+
+}
+
+class DameLaHora implements ActionListener{
+	
+	public void actionPerformed(ActionEvent e) {
+		
+		Date ahora = new Date();
+		
+		System.out.println("Te pongo la hora cada 5 sg: " + ahora);
+		
+		
+	}
+	
+}
